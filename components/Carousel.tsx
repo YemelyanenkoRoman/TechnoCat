965dc50eae5013745f27c55ccab28b5284c7a37a
YemelyanenkoRoman <yemelyanenko.roman@gmail.com>
2023-07-11 14:59:38 +0300

    created a carousel in discont

---
+++ components/Carousel.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { discounts } from './Discount';
+import { useState, useEffect } from 'react';
+
+export default function Carousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % discounts.length);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % discounts.length);
+  };
+
+  const handleSelectSlide = (index: number) => {
+    setCurrentSlide(index);
+  };
+
+  return (
+    <div className="p-[35px] flex justify-between relative" onClick={handleNextSlide} style={{ cursor: 'pointer' }}>
+      {discounts.map((discount, index) => (
+        <div
+          key={index}
+          style={{
+            transition: 'opacity 1s ease-in-out',
+            opacity: currentSlide === index ? 1 : 0,
+            zIndex: currentSlide === index ? 1 : 0,
+          }}
+          className="absolute inset-0 flex"
+        >
+          <div className="static flex justify-between">
+            <figure className="text-thirtyfour">{discount.img}</figure>
+            <div className=" w-[448px] ml-[30px] flex flex-col justify-center items-center">
+              <h3 className="text-thirtyfour">{discount.title}</h3>
+              <p className="font-poppins text-sexteen pt-[25px]">{discount.textOne}</p>
+              <p className="font-poppins text-sexteen pt-[15px]">{discount.textTwo}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div
+        className="flex justify-center space-x-2 absolute bottom-0 left-0 right-0 mb-4 z-50"
+        style={{
+          right: '-190px',
+        }}
+      >
+        {discounts.map((_, index) => (
+          <button
+            key={index}
+            onClick={(event) => {
+              event.stopPropagation();
+              handleSelectSlide(index);
+            }}
+            style={{
+              width: 8,
+              height: 8,
+              backgroundColor: currentSlide === index ? '#718096' : '#cbd5e0',
+              borderRadius: '50%',
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- components/Discount.tsx
+++ components/Discount.tsx
@@ -1,40 +1,45 @@
 import DiscountImg from '../public/discount/discount.svg';
+import DiscountImg2 from '../public/discount/discount2.svg';
+import DiscountImg3 from '../public/discount/discount3.svg';
+import Carousel from './Carousel';
 import Form from './FormDiscount';
 
-interface Discount {
+export interface Discount {
   img: JSX.Element;
   title: string;
   textOne: string;
   textTwo?: string;
 }
 
-const DiscountObj: Discount = {
-  img: <DiscountImg />,
-  title: 'Получите дополнительную скидку на обучение 20%',
-  textOne:
-    'Скидка предоставляется новым клиентам на первое занятие, оформившим заявку на обучение через наш сайт. ',
-  textTwo:
-    'Заполните форму и наши специалисты свяжутся с вами в течение рабочего дня.',
-};
+export const discounts: Discount[] = [
+  {
+    img: <DiscountImg />,
+    title: 'Учитесь больше — платите меньше!',
+    textOne:
+      'Если ваш ребенок ходит на два направления в нашем образовательном центре, то один из них вы можете получить со скидкой 10%.',
+    textTwo: 'Спешите записаться на интересующие вас направления и получите выгодное предложение.',
+  },
+  {
+    img: <DiscountImg2 />,
+    title: 'Семейная скидка в нашем образовательном центре!',
+    textOne:
+      'Если у вас двое детей, которые хотят посещать занятия в нашем образовательном центре, то мы дарим вам 10% скидку на одно из них.',
+    textTwo: 'Не упустите шанс дать своим детям качественное образование по доступной цене.',
+  },
+  {
+    img: <DiscountImg3 />,
+    title: 'Делитесь знаниями с друзьями и экономьте!',
+    textOne:
+      'Если вы приведете друга в наш образовательный центр, то вы и он получите по 10% скидку на любой курс на целый месяц',
+    textTwo: 'Не упусти возможность учиться вместе с теми, кто вам близок, и при этом сэкономить.',
+  },
+];
 
 const Discount = () => {
   return (
     <div className="m-auto max-w-[1121px] bg-local-gray-act rounded-lg">
       <div className="p-[35px] flex justify-between">
-        <figure className="text-thirtyfour">{DiscountObj.img}</figure>
-        <div className="max-w-[448px] flex flex-col justify-center items-center">
-          <h3 className=" text-thirtyfour">
-            Получите дополнительную скидку на обучение 20%
-          </h3>
-          <p className="font-poppins text-sexteen pt-[25px]">
-            Скидка предоставляется новым клиентам на первое занятие, оформившим
-            заявку на обучение через наш сайт.{' '}
-          </p>
-          <p className="font-poppins text-sexteen pt-[15px]">
-            Заполните форму и наши специалисты свяжутся с вами в течение
-            рабочего дня.
-          </p>
-        </div>
+        <Carousel />
         <div className="flex justify-center w-[280px] h-[248px]">
           <Form />
         </div>
