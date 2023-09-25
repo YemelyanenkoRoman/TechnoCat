6b8be88356e5066c9b41e6fb6f30a0854a29faf6
Ivan Emelyanenko <ivanemelyanenko@ip-172-16-1-27.eu-central-1.compute.internal>
2023-09-25 20:27:01 +0300

    Fix check width

--- utils/hooks/useWindowWidth.ts
+++ utils/hooks/useWindowWidth.ts
@@ -1,24 +1,36 @@
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { headers } from "next/dist/client/components/headers";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 export const useWindowWidth = () => {
   const [width, setWidth] = useState(() => {
-    return typeof window === 'undefined' ? 1500 : window.innerWidth;
+    if (typeof window === "undefined") {
+      const headersList = headers();
+      const userAgent = headersList.get("user-agent");
+      const isMobileView = !!userAgent?.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+      );
+
+      return isMobileView ? 0 : 2000;
+    }
+
+    return window.innerWidth;
   });
 
-  const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+  const useIsomorphicLayoutEffect =
+    typeof window !== "undefined" ? useLayoutEffect : useEffect;
 
   useIsomorphicLayoutEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const updateWidth = () => {
         setWidth(window.innerWidth);
       };
 
       updateWidth();
 
-      window.addEventListener('resize', updateWidth);
+      window.addEventListener("resize", updateWidth);
 
       return () => {
-        window.removeEventListener('resize', updateWidth);
+        window.removeEventListener("resize", updateWidth);
       };
     }
   }, []);
